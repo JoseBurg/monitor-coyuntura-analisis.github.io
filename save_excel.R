d038e2c2c4569d448af8ac1866c78539e53ddcf4
José Antonio Burgos Francisco <65372353+JoseBurg@users.noreply.github.com>
2025-11-20 16:21:29 -0400

    Comment out 'ipc-general' worksheet and data writing
    Comment out the addition of the 'ipc-general' worksheet and its data writing.

--- save_excel.R
+++ save_excel.R
@@ -7,12 +7,12 @@ datos_coyuntura <- createWorkbook()
 addWorksheet(datos_coyuntura, "ipp-manufactura")
 addWorksheet(datos_coyuntura, "ipp-servicio")
 addWorksheet(datos_coyuntura, "imae")
-addWorksheet(datos_coyuntura, "ipc-general")
+# addWorksheet(datos_coyuntura, "ipc-general")
 
 writeData(datos_coyuntura, "ipp-manufactura", get_ipp())
 writeData(datos_coyuntura, "ipp-servicio", get_ipp(FALSE))
 writeData(datos_coyuntura, "imae", get_imae())
-writeData(datos_coyuntura, "ipc-general", databcrd::get_ipc_data(desagregacion = "general"))
+# writeData(datos_coyuntura, "ipc-general", databcrd::get_ipc_data(desagregacion = "general"))
 
 saveWorkbook(datos_coyuntura, file = "data-coyuntura.xlsx", overwrite = TRUE)
 
